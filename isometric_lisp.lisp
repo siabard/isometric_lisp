4eb20ddf1f77bd75bd92a2f8e6ff917ecd197728
Yeonho Jang <siabard@gmail.com>
2022-05-02 21:02:05 +0900

    Add draw & update

--- isometric_lisp.lisp
+++ isometric_lisp.lisp
@@ -2,6 +2,11 @@
 
 (in-package #:isometric_lisp)
 
+(defun draw (renderer)
+  (sdl2:set-render-draw-color renderer 155 59 155 255)
+  (sdl2:render-clear renderer)
+  (sdl2:render-present renderer))
+
 (defun main ()
   (sdl2:with-init (:everything)
     (sdl2:with-window (win :title "Isometric" :flags '(:shown))
@@ -12,9 +17,7 @@
 		  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
 		    (sdl2:push-event :quit)))
 	  (:idle ()
-		 (sdl2:set-render-draw-color renderer 0 0 0 255)
-		 (sdl2:render-clear renderer)
-		 (sdl2:render-present renderer))
+		 (draw renderer))
 	  (:quit ()
 		 (progn
 		   (sdl2-image:quit)
